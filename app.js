ee9156efc2e5d6e5338fcf48874bfc8580d8acff
pedrocerebro <elcerebro@MacBook-Pro-de-pedro.local>
2022-04-11 18:21:04 -0300

    dolor por rap

--- app.js
+++ app.js
@@ -9,3 +9,16 @@ app.listen(3003,()=>{
 app.get("/",(req,res)=>{
     res.sendFile(path.resolve(__dirname,"./views/home.html"))
 })
+
+app.get("/registro",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/register.html"))
+})
+app.post("/registro",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/home.html"))
+})
+app.get("/login",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/login.html"))
+})
+app.post("/login",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/login.html"))
+})
