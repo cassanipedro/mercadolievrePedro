8b9a9959eef0d0a10e72d6708e24fd4eada7c361
cassanipedro <cassanipedro01@gmail.com>
2022-04-15 03:01:55 -0300

    ayuda

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express=require("express")
-const app=express()
-
-const path=require("path")
+const app=express();
+const path=require("path");
 app.use(express.static(path.resolve(__dirname,"./public")));
-app.listen(3003,()=>{
-    console.log("servidor corriendo en http://localhost:3003")
+app.listen(process.env.PORT || 3003, ()=>{
+    console.log("http://localhost:3003")
 })
 app.get("/",(req,res)=>{
     res.sendFile(path.resolve(__dirname,"./views/home.html"))
